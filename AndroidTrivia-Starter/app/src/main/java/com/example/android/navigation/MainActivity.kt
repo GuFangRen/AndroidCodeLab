3d6853bd30778c335c9f39a247d4ac52606a3dab
zhuxiaolong.719 <zhuxiaolong.719@bytedance.com>
2022-04-13 14:27:04 +0800

    5.1 ViewModel done

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,13 +19,33 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.ActivityMainBinding
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        drawerLayout = binding.drawerLayout
+
+        val navController=this.findNavController(R.id.myNavHostFragment)
+        //设置返回键UI的，并把返回键关联到navController
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        //设置滑动菜单UI，并把滑动菜单关联到nacController
+        NavigationUI.setupWithNavController(binding.navView,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController=this.findNavController(R.id.myNavHostFragment)
+        //return navController.navigateUp()
+        return NavigationUI.navigateUp(navController,drawerLayout)
     }
 
     // TODO (01) Create the new TitleFragment
